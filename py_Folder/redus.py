b6ef6b1e7d6d149a4af2982a0c1831e5cc0e1664
emilhristov000-jpg <emilhristov000@gmail.com>
2026-09-14 17:50:20 +0200

    Second page html css next step optimizing css.css

--- py_Folder/redus.py
+++ py_Folder/redus.py
@@ -1,14 +1,14 @@
 import redis
 import subprocess
-
+import time
 
 def redis_start():
-    try:
-        r = redis.Redis(
+    r = redis.Redis(
             host="localhost",
             port=6379,
             decode_responses=True
         )
+    try:
 
         r.ping()
         print("Redis is already running.")
@@ -17,21 +17,24 @@ def redis_start():
     except redis.exceptions.ConnectionError:
         print("Redis isn't running. Starting Docker container...")
 
-        subprocess.run([
+        wow =subprocess.run([
             "docker", "start", "redis"
-        ], check=False)
+        ], capture_output=True, text=True)
 
-        r = redis.Redis(
-            host="localhost",
-            port=6379,
-            decode_responses=True
-        )
-
-        try:
-            r.ping()
-            print("Redis started successfully.")
-            return r
-
-        except redis.exceptions.ConnectionError:
-            print("Could not start Redis.")
+        if wow.returncode != 0:
+            print(f"Failed to start container: {wow.stderr.strip()}")
             return None
+
+        for _ in range(5):
+                time.sleep(1)
+                try:
+                    r.ping()
+                    print("Redis started successfully.")
+                    return r
+                except redis.exceptions.ConnectionError:
+                    continue
+
+        print("Could not connect to Redis after container start.")
+        return None
+
+redis_start()
